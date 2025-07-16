78a8db1d944f1a3bfbd3421c05a26fc0e08e0da4
LiChenyang-GZ <1169281314@qq.com>
2025-07-16 19:39:03 +1000

    fix some page bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-black`}
+        className={`${geistSans.variable} ${geistMono.variable} fixed antialiased bg-white text-black min-h-screen w-full`}
       >
         {children}
       </body>
