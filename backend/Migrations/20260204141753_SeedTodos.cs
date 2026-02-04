273e5bc0d39582aee0c09d64e54b72eb11db0b9e
LiChenyang-GZ <1169281314@qq.com>
2026-02-05 01:21:41 +1100

    seed data

---
+++ backend/Migrations/20260204141753_SeedTodos.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedTodos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Todos",
+                columns: new[] { "Id", "Completed", "Description", "Text" },
+                values: new object[,]
+                {
+                    { 1, false, "Milk, eggs, bread", "Buy groceries" },
+                    { 2, false, "Monthly sales report", "Write report" },
+                    { 3, true, "Evening walk", "Walk dog" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
