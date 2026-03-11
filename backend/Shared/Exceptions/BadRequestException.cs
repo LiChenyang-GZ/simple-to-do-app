b242ae6486f4c1e00e58535050c2974eb71cea52
LiChenyang-GZ <1169281314@qq.com>
2026-03-12 01:26:44 +1100

    solve conflicts

--- backend/Shared/Exceptions/BadRequestException.cs
+++ backend/Shared/Exceptions/BadRequestException.cs
@@ -2,5 +2,7 @@ namespace Backend.Shared.Exceptions;
 
 public class BadRequestException : Exception
 {
-    public BadRequestException(string message) : base(message) { }
+    public BadRequestException(string message) : base(message)
+    {
+    }
 }
--- backend/Shared/Exceptions/NotFoundException.cs
+++ backend/Shared/Exceptions/NotFoundException.cs
@@ -2,8 +2,7 @@ namespace Backend.Shared.Exceptions;
 
 public class NotFoundException : Exception
 {
-    public NotFoundException(string message) : base(message) { }
-
-    public NotFoundException(string entityName, object key)
-        : base($"{entityName} with id '{key}' was not found.") { }
+    public NotFoundException(string message) : base(message)
+    {
+    }
 }
